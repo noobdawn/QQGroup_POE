13446d2f8639c72dfc5d845d9e4c49a19db6f44e
noobdawn <694109410@qq.com>
2019-07-25 02:34:02 +0800

    大盘

--- NiLiuShui.IRQQ.CSharp/Stock.cs
+++ NiLiuShui.IRQQ.CSharp/Stock.cs
@@ -17,32 +17,82 @@ namespace NiLiuShui.IRQQ.CSharp
         public Stock()
         {
             time = 0;
-            MirrorInit();
+            ExStockInit();
             Timeslice.AddSecondEvent(StockStep);
         }
 
         #region 崇高石市场
-        public const int MaxExPrice = 150;
-        public const int MinExPrice = 50;
+        private int octaves = 2;
+        private double persistence = 0.5;
+
+        /// <summary>
+        /// 分形用的玩意儿
+        /// </summary>
+        /// <param name="px"></param>
+        /// <returns></returns>
+        private double Random1D(int px)
+        {
+            int n = px;
+            n = (n << 13) ^ n;
+            
+            return (1f - (n * (n * n * 15731 + 789221) + 1376312589 & 0x7fffffff) / 1073741824.0);
+        }
+        private double Smooth1D(int x)
+        {
+            //return Random1D(x);
+
+            double res = Random1D(x) * 0.6666;
+            res += (Random1D(x - 1) + Random1D(x + 1)) * 0.3334;
+            return res;
+        }
+        private double cosine_interpolate(double a, double b, double x)
+        {
+            double ft = x * Math.PI;
+            double f = (1 - Math.Cos(ft)) * 0.5f;
+            return a * (1 - f) + b * f;
+        }
+        private double InterpolatedNoise(double x)
+        {
+            int ix = (int)x;
+            double fx = x - ix;
+            return cosine_interpolate(Smooth1D(ix), Smooth1D(ix + 1), fx);
+        }
+        private double FractalRandom(double x)
+        {
+            var total = 0.0;
+            for (int i = 0; i < octaves; i++)
+            {
+                var frequency = Math.Pow(2, i);
+                var amplitude = Math.Pow(persistence, i);
+                total += InterpolatedNoise(x * frequency) * amplitude;
+            }
+            return total;
+        }
+        
         public const int MiddleExPrice = 100;
+        //由股市和散户分别可能造成的波动幅度
+        public const int StockTrendCount = 50;
+        public const int IOTrendCount = 50;
         private int curExPrice;
         //上一时间段内总的流入流出量
         private int lastStepIO;
-        //下一时间段原本待定的涨幅
-        private int trend;
+        private double xxx;
 
         //开盘
         private void ExStockInit()
         {
             curExPrice = MiddleExPrice;
             lastStepIO = 0;
-            trend = 1000;
+            xxx = DateTime.Now.Millisecond + DateTime.Now.Second;
         }
 
         //每两分钟发生的变化
         private void ExStockStep()
         {
-
+            StringBuilder sb = new StringBuilder();
+            curExPrice = (int)(MiddleExPrice + StockTrendCount * FractalRandom(xxx));
+            xxx += 0.1;
+            lastStepIO = 0;
         }
 
         /// <summary>
@@ -51,7 +101,7 @@ namespace NiLiuShui.IRQQ.CSharp
         /// <param name="delta"></param>
         private void ExStockChange(int delta)
         {
-
+            lastStepIO += delta;
         }
         #endregion
 
@@ -139,12 +189,11 @@ namespace NiLiuShui.IRQQ.CSharp
         private void StockStep(object sender, ElapsedEventArgs e)
         {
             time++;
-            if (time >= 20)
+            if (time >= 5)
             {
-                MirrorUpdate();
-                if (done)
-                    _S.Broadcast("Response_to_QueryStock", intro);
-                time -= 20;
+                ExStockStep();
+                _S.Broadcast(curExPrice.ToString());
+                time -= 5;
             }
         }
 
@@ -153,55 +202,55 @@ namespace NiLiuShui.IRQQ.CSharp
             if (!done) return true;
             if (param.param[0] == _S.GetText("Command_to_QueryStock") && param.param.Length == 1)
             {
-                //_S.Response(param.GroupQQ, param.QQ, "Response_to_QueryStock", curPrice);
-                _S.Response(param.GroupQQ, param.QQ, intro);
+                _S.Response(param.GroupQQ, param.QQ, "Response_to_QueryStock", curExPrice);
+                //_S.Response(param.GroupQQ, param.QQ, intro);
                 return false;
             }
-            //int opCnt;
-            //int realCost;
-            //if (param.param[0] == _S.GetText("Command_to_Buy") && param.param.Length == 1)
-            //{
-            //    opCnt = (int)Math.Floor(param.caster.ChaosCount / curPrice);
-            //    realCost = opCnt * curPrice;
-            //    if (_S.Cost(param.caster, realCost))
-            //    {
-            //        DataRunTime.ExChange(param.GroupQQ, param.QQ, opCnt);
-            //        _S.Response(param.GroupQQ, param.QQ, "Response_to_StockOp", param.caster.QQ, param.caster.ChaosCount, param.caster.ExCount);
-            //    }
-            //    return false;
-            //}
-            //if (param.param[0] == _S.GetText("Command_to_Buy") && param.param.Length == 2 && int.TryParse(param.param[1], out opCnt))
-            //{
-            //    if (opCnt <= 0) return false;
-            //    realCost = opCnt * curPrice;
-            //    if (_S.Cost(param.caster, realCost))
-            //    {
-            //        DataRunTime.ExChange(param.GroupQQ, param.QQ, opCnt);
-            //        _S.Response(param.GroupQQ, param.QQ, "Response_to_StockOp", param.caster.QQ, param.caster.ChaosCount, param.caster.ExCount);
-            //    }
-            //    return false;
-            //}
-            //if (param.param[0] == _S.GetText("Command_to_Sell") && param.param.Length == 1)
-            //{
-            //    opCnt = param.caster.ExCount;
-            //    opCnt = -opCnt;
-            //    realCost = Math.Abs(opCnt * curPrice);
-            //    DataRunTime.ExChange(param.GroupQQ, param.QQ, opCnt);
-            //    DataRunTime.ChaosChange(param.GroupQQ, param.QQ, realCost);
-            //    _S.Response(param.GroupQQ, param.QQ, "Response_to_StockOp", param.caster.QQ, param.caster.ChaosCount, param.caster.ExCount);
-            //    return false;
-            //}
-            //if (param.param[0] == _S.GetText("Command_to_Sell") && param.param.Length == 2 && int.TryParse(param.param[1], out opCnt))
-            //{
-            //    if (opCnt <= 0) return false;
-            //    if (opCnt > param.caster.ExCount) return false;
-            //    opCnt = -opCnt;
-            //    realCost = Math.Abs(opCnt * curPrice);
-            //    DataRunTime.ExChange(param.GroupQQ, param.QQ, opCnt);
-            //    DataRunTime.ChaosChange(param.GroupQQ, param.QQ, realCost);
-            //    _S.Response(param.GroupQQ, param.QQ, "Response_to_StockOp", param.caster.QQ, param.caster.ChaosCount, param.caster.ExCount);
-            //    return false;
-            //}
+            int opCnt;
+            int realCost;
+            if (param.param[0] == _S.GetText("Command_to_Buy") && param.param.Length == 1)
+            {
+                opCnt = (int)Math.Floor(param.caster.ChaosCount / curExPrice);
+                realCost = opCnt * curExPrice;
+                if (_S.Cost(param.caster, realCost))
+                {
+                    DataRunTime.ExChange(param.GroupQQ, param.QQ, opCnt);
+                    _S.Response(param.GroupQQ, param.QQ, "Response_to_StockOp", param.caster.QQ, param.caster.ChaosCount, param.caster.ExCount);
+                }
+                return false;
+            }
+            if (param.param[0] == _S.GetText("Command_to_Buy") && param.param.Length == 2 && int.TryParse(param.param[1], out opCnt))
+            {
+                if (opCnt <= 0) return false;
+                realCost = opCnt * curExPrice;
+                if (_S.Cost(param.caster, realCost))
+                {
+                    DataRunTime.ExChange(param.GroupQQ, param.QQ, opCnt);
+                    _S.Response(param.GroupQQ, param.QQ, "Response_to_StockOp", param.caster.QQ, param.caster.ChaosCount, param.caster.ExCount);
+                }
+                return false;
+            }
+            if (param.param[0] == _S.GetText("Command_to_Sell") && param.param.Length == 1)
+            {
+                opCnt = param.caster.ExCount;
+                opCnt = -opCnt;
+                realCost = Math.Abs(opCnt * curExPrice);
+                DataRunTime.ExChange(param.GroupQQ, param.QQ, opCnt);
+                DataRunTime.ChaosChange(param.GroupQQ, param.QQ, realCost);
+                _S.Response(param.GroupQQ, param.QQ, "Response_to_StockOp", param.caster.QQ, param.caster.ChaosCount, param.caster.ExCount);
+                return false;
+            }
+            if (param.param[0] == _S.GetText("Command_to_Sell") && param.param.Length == 2 && int.TryParse(param.param[1], out opCnt))
+            {
+                if (opCnt <= 0) return false;
+                if (opCnt > param.caster.ExCount) return false;
+                opCnt = -opCnt;
+                realCost = Math.Abs(opCnt * curExPrice);
+                DataRunTime.ExChange(param.GroupQQ, param.QQ, opCnt);
+                DataRunTime.ChaosChange(param.GroupQQ, param.QQ, realCost);
+                _S.Response(param.GroupQQ, param.QQ, "Response_to_StockOp", param.caster.QQ, param.caster.ChaosCount, param.caster.ExCount);
+                return false;
+            }
             return true;
         }
 
